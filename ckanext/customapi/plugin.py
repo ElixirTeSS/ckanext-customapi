55cd0636d51a886564c5bcfd0b7adbc9c88edaa2
Milo Thurston <knirirr@gmail.com>
2015-01-20 15:18:32 +0000

    Works, but saved format of the iann json feed is dodgy.

--- ckanext/customapi/plugin.py
+++ ckanext/customapi/plugin.py
@@ -1,3 +1,4 @@
+import json as json
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
@@ -5,8 +6,12 @@ import ckan.plugins.toolkit as toolkit
 @toolkit.side_effect_free
 def get_news(context,data_dict=None):
   # The actual custom API method
-  return {"hello":"world"}
-
+  #return {"hello":"world"}
+  with open('/tmp/iann_events.json') as data_file:
+    data = json.load(data_file)
+    with open('/tmp/iann_events.json') as data_file:
+      data = json.load(data_file)
+   return(data)
 
 class CustomAPIPlugin(plugins.SingletonPlugin):
   plugins.implements(plugins.interfaces.IActions)
@@ -14,4 +19,3 @@ class CustomAPIPlugin(plugins.SingletonPlugin):
   def get_actions(self):
     # Registers the custom API method defined above
     return {'get_news': get_news}
-
